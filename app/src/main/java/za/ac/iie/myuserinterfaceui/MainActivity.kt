bcd5966a82ade0660f15d6446e995fc8e66cf901
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-03-31 18:14:24 +0200

    Added action the clickMe button to display the personalize greeting

--- app/src/main/java/za/ac/iie/myuserinterfaceui/MainActivity.kt
+++ app/src/main/java/za/ac/iie/myuserinterfaceui/MainActivity.kt
@@ -2,24 +2,34 @@ package za.ac.iie.myuserinterfaceui
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        // declaring all the conponents located in the user interface
+        // declaring all the components located in the user interface
         var clicMebutton=findViewById<Button>(R.id.buttonEnter)
         var zulu=findViewById<Switch>(R.id.zuluSwitch)
+        var greetingText=findViewById<TextView>(R.id.textViewone)
+        var inputText=findViewById<EditText>(R.id.editTextInput)
         // assignating a toast message to the button "buttonEnter" when clicked
         clicMebutton.setOnClickListener {
             Toast.makeText(this@MainActivity, "button clicked", Toast.LENGTH_SHORT).show()
+            //configurated the textview so that the greeting can contain the name of the user
+            greetingText.text="welcome, ${inputText}!"
+
         }
 
 
